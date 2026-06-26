f76706e256d992e2084d00715bcb6a45068d5e9d
池ノ谷優太 <you1207.ike@gmail.com>
2026-06-26 14:54:34 +0900

    variable修正

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -8,3 +8,21 @@ variable "region" {
   description = "OCIリージョン名 (例: ap-tokyo-1)"
   type        = string
 }
+
+variable "user_ocid" {
+  description = "ユーザーID"
+  type        = string
+  sensitive   = true
+}
+
+variable "private_key" {
+  description = "プライベートキー"
+  type        = string
+  sensitive   = true
+}
+
+variable "fingerprint" {
+  description = "フィンガープリント"
+  type        = string
+  sensitive   = true
+}
